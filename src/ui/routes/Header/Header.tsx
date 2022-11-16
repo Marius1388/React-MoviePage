a7b566aa754c28557c2b00e1821adee290f5dcbe
Marius1388 <mariuscristian.bancila@gmail.com>
2022-11-17 00:18:33 +0200

    implemented footer

--- src/ui/routes/Header/Header.tsx
+++ src/ui/routes/Header/Header.tsx
@@ -2,6 +2,8 @@ import { Outlet, Link } from 'react-router-dom';
 import logo from '../../../assets/logo/deadful.svg';
 import clapper from '../../../assets/icon/clapperboard.svg';
 import tv from '../../../assets/icon/tv.svg';
+import app from '../../../assets/button/app-store.svg';
+import google from '../../../assets/button/google-play.svg';
 import './Header.styles.scss';
 
 export function Header() {
@@ -9,11 +11,7 @@ export function Header() {
 		<>
 			<header className="header">
 				<Link className="logo-container" to="/">
-					<img
-						src={logo}
-						className="logo-container"
-						alt="dreadful Tomatoes logo"
-					/>
+					<img src={logo} alt="dreadful Tomatoes logo" />
 				</Link>
 
 				<div className="nav-links-container">
@@ -38,6 +36,26 @@ export function Header() {
 				</div>
 			</header>
 			<Outlet />
+			<footer className="footer">
+				<Link className="logo-container" to="/">
+					<img src={logo} alt="dreadful Tomatoes logo" />
+				</Link>
+				<div className="buttons-container">
+					<img
+						width={150}
+						src={app}
+						className="button-container"
+						alt="clapper"
+					/>
+					<img
+						width={150}
+						src={google}
+						className="button-container"
+						alt="clapper"
+					/>
+				</div>
+				<p>© Copyright 2022 Dreadful Tomatoes. All rights reserved.</p>
+			</footer>
 		</>
 	);
 }
