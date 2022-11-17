3f61870fbb80ef6b129290a0a67a0d8fec4ef100
Marius1388 <mariuscristian.bancila@gmail.com>
2022-11-17 03:09:02 +0200

    home page done

---
+++ src/services/DataService.ts
@@ -0,0 +1,26 @@
+type ProgramType = 'movies' | 'series'
+export const getData = async (programType?: ProgramType) => {
+    const response = await fetch('https://static.rviewer.io/challenges/datasets/dreadful-tomatoes/data.json')
+    const data = await response.json()
+    let result: any
+    if (!programType) {
+        result = data.entries
+    } else {
+        result = data.entries.filter(obj => {
+            obj.programType === programType
+        })
+    }
+    return result
+}
+export const getHomePageResults = async () => {
+    const response = await fetch('https://static.rviewer.io/challenges/datasets/dreadful-tomatoes/data.json')
+    const data = await response.json()
+    const movie = data.entries.filter(obj =>
+        obj.title.toLowerCase().includes('avengers')
+    )[0]
+    const series = data.entries.filter(obj =>
+        obj.title.toLowerCase().includes('thrones')
+    )[0]
+
+    return [movie, series]
+}
--- src/ui/routes/Home/Home.tsx
+++ src/ui/routes/Home/Home.tsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from 'react';
 import './Home.styles.scss';
+import clapper from '../../../assets/icon/clapperboard.svg';
+import tv from '../../../assets/icon/tv.svg';
+import { getHomePageResults } from '../../../services/DataService';
+import { Link } from 'react-router-dom';
 
 export function Home() {
+	let [data, setData] = useState<any[]>([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			const res: any[] = await getHomePageResults();
+			setData(res);
+		};
+		fetchData();
+	}, []);
+
 	return (
 		<div className="home">
-			<p>
-				Here you would find a React bootstrapped webapp where you could
-				start coding without taking care about configuring so many things,
-				and just focus on the code!
-			</p>
-			<p>
-				You could start modifying the <code>src/App.tsx</code> file to see
-				any change in the webapp.
-			</p>
+			{data.map((obj, index) => {
+				return (
+					<div key={index} className="poster">
+						<img src={obj.images.posterArt.url} alt={obj.title} />
+						{obj.programType === 'movies' ? (
+							<Link className="banner" to="/movies">
+								<img width={20} src={clapper} alt="clapper" />
+								<p>Movies</p>
+							</Link>
+						) : (
+							<Link className="banner" to="/series">
+								<img width={20} src={tv} alt="tv" />
+								<p>Series</p>
+							</Link>
+						)}
+					</div>
+				);
+			})}
 		</div>
 	);
 }
